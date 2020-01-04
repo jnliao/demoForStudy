f19921d8307b9f71839e389a7604488e98568c61
jinneng.liao <jnliao116@163.com>
2020-01-04 16:38:57 +0800

    emoji

---
+++ src/main/java/com/hn/liao/emoji/EmojiMainDemo.java
@@ -0,0 +1,29 @@
+package com.hn.liao.emoji;
+
+import com.vdurmont.emoji.EmojiParser;
+
+import java.util.List;
+
+/**
+ * @author liaojinneng
+ * @date 2020/1/4
+ */
+public class EmojiMainDemo {
+
+    public static void main(String[] args){
+        removeAllEmojis();
+        extractEmojis();
+    }
+
+    private static void removeAllEmojis(){
+        String str = "An 😀awesome 😃string with a few 😉emojis 小孩\uD83D\uDE1C \uD83C\uDF3C \uD83D\uDC37\uD83D\uDC37 🐑 🐴";
+        String strNew = EmojiParser.removeAllEmojis(str);
+        System.out.println(strNew);
+    }
+
+    private static void extractEmojis(){
+        String str = "An 😀awesome 😃string with a few 😉emojis 小孩\uD83D\uDE1C \uD83C\uDF3C \uD83D\uDC37\uD83D\uDC37 🐑 🐴";
+        List<String> stringList = EmojiParser.extractEmojis(str);
+        System.out.println(stringList);
+    }
+}
