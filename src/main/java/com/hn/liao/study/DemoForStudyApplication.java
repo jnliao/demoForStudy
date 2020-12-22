8b3a6e9c76569ea67079d51fda0172ebb13358fc
liaojinneng <jinneng.liao@zhangmen.com>
2020-12-22 09:13:19 +0800

    引入spring boot框架

---
+++ src/main/java/com/hn/liao/study/DemoForStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hn.liao.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoForStudyApplication.class, args);
+    }
+
+}
